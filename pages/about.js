b553fff37c4c01cead971750604cba9df8f50c71
Baeyeonggi <byg5913@gmail.com>
2021-02-10 18:34:54 +0900

    npx creat-next-app with article

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "next/head";
+
+const about = () => {
+  return (
+    <div>
+      <Header>
+        <title>About</title>
+      </Header>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default about;
